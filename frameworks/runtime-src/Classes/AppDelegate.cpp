f3d8ba98ff592f6edd3dd1d1dcd8f9da9fe48d41
Zhiyuan <zyguochn@gmail.com>
2017-06-07 18:20:12 +0800

    add url protocal support

--- frameworks/runtime-src/Classes/AppDelegate.cpp
+++ frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -18,14 +18,16 @@
 #include "ParaScriptBindings/local_bridge_manual.h"
 #include "AutoUpdate/AutoUpdater.h"
 #include "ParaXModel/FBXParser.h"
+
+#include <regex>
 using namespace NPL;
 /** define this if you want to use cocos API with NPL. If you just use NPL, undefine this. */
 // #define USE_COCOS_LUA_ENGINE_AS_MAIN_NPL_STATE
 
-AppDelegate::AppDelegate()
+AppDelegate::AppDelegate(std::string worldUrl)
 	:m_sScriptSearchPath("src")
 {
-	
+	m_worldUrl = worldUrl;
 }
 
 AppDelegate::~AppDelegate()
@@ -61,14 +63,48 @@ void ParaEngine::AppDelegate::CreateParaEngineApp()
 	std::string sCmdLine;
 
 	// read config file
+	std::string dir = CParaFile::GetCurDirectory(0);
+	OUTPUT_LOG("world url: %s\n", m_worldUrl.c_str());
+	OUTPUT_LOG("current directory: %s\n", dir.c_str());
 	CParaFile configFile;
 	if (configFile.OpenFile("config.txt", true, NULL, false, FILE_ON_DISK))
 	{
 		configFile.GetNextAttribute("cmdline", sCmdLine);
 		configFile.close();
 	}
-	if (sCmdLine.empty())
+	OUTPUT_LOG("sCmdLine: %s\n", sCmdLine.c_str());
+	if (!sCmdLine.empty())
+	{
+		// concat the world url if there is any
+		OUTPUT_LOG("enter: \n");
+		if (!m_worldUrl.empty())
+		{
+			OUTPUT_LOG("enter2: \n");
+			//try {
+			//	std::regex re("paracraft://cmd/loadworld (.*)");
+			//	std::smatch match;
+			//	OUTPUT_LOG("world url:%s\n", m_worldUrl.c_str());
+			//	if (std::regex_search(m_worldUrl, match, re) && match.size() > 1)
+			//	{
+			//		OUTPUT_LOG("matched");
+			//		std::string url = match.str(1);
+			//		sCmdLine += " world=\"" + url + "\"";
+			//		OUTPUT_LOG(sCmdLine.c_str());
+			//	}
+			//	OUTPUT_LOG("enter3: \n");
+			//}
+			//catch (std::regex_error& e) {
+			//	OUTPUT_LOG("enter4: \n");
+			//}
+			std::string url = m_worldUrl.substr(26);
+			sCmdLine += " world=\"" + url + "\"";
+			OUTPUT_LOG(sCmdLine.c_str());
+		}
+	}
+	else
+	{
 		GetCommandLine(sCmdLine);
+	}		
 	m_pParaEngineApp.reset(new CParaEngineApp(sCmdLine.c_str()));
 }
 
